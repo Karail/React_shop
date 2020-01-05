7abbc83da7d87355241645b8deb545aaf96e04b2
Ilkara <karail-fokus@mail.ru>
2020-01-06 02:48:27 +0300

    test2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,29 +1,7 @@
 import React from 'react';
-import { Menu, List, Button, Image, Popup } from 'semantic-ui-react'
+import { Menu, Popup } from 'semantic-ui-react'
+import CartComponent from './CartComponent'
 
-class CartComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-  render() {
-    return (
-      <List selection divided verticalAlign='middle'>
-
-      <List.Item>
-        <List.Content floated='right'>
-          <Button onClick={this.props.removeFromCart.bind(this, this.props.id)} color="red">удалить</Button>
-        </List.Content>
-        <Image avatar src={this.props.image} />
-          <List.Content>{this.props.title}</List.Content>
-      </List.Item>
-  
-    </List>
-    );
-  }
-}
 
 class MenuComponent extends React.Component {
   constructor(props) {
