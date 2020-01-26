9227029af6108b9484ff6f89a36dc90b5d1c9da4
Ilkara <karail-fokus@mail.ru>
2020-01-27 00:30:24 +0300

    local_storage

--- src/store.js
+++ src/store.js
@@ -4,6 +4,9 @@ import logger from 'redux-logger';
 import rootReducer from './reducers';
 
 export default () => {
-    const store = createStore(rootReducer, applyMiddleware(logger))
+    const store = createStore(rootReducer,  (localStorage['redux-store']) ? JSON.parse(localStorage['redux-store']) : {} )
+
+    store.subscribe(() => {  localStorage['redux-store'] = JSON.stringify(store.getState()) })
+    
     return store;
 }
